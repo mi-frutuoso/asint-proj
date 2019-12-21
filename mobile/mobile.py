a28d681c8483dc389c1f2e6212415d0ae9b78eb3
mi-frutuoso <mariacfines@gmail.com>
2019-12-21 03:43:05 +0000

    mobile: user validation working with errors

--- mobile/mobile.py
+++ mobile/mobile.py
@@ -8,6 +8,10 @@ import json
 import requests
 from users import User
 
+import string
+import random
+import threading
+
 redirect_uri = "http://127.0.0.1:5000/userAuth" # this is the address of the page on this app
 
 
@@ -77,6 +81,95 @@ def validate():
 
 	return render_template("validation.html", username=name, key=key, photo_data = photo_data, photo_type=photo_type)	
 
+secret_submitted = threading.Event()
+responsible_user = None
+responsible_photoData = None
+responsible_photoType = None
+
+
+# request secret
+@app.route('/validation/request')
+def val_request():
+
+	name, key, photo_data, photo_type = Authentication()
+	# generate secret until it is unique
+	flag_continue=0
+	while True:
+		secret = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(7))
+		for user in user_list:
+			if user.secret==secret:
+				flag_continue=1
+				break
+		if(flag_continue):
+			continue
+		else:
+			break
+	
+	for user in user_list:
+		if user.key==key:
+			user.updateSecret(secret)
+			break
+	# "wait" for some user to request the secret
+	return render_template("request.html", username=name, key=key, photo_data = photo_data, photo_type=photo_type, secret=secret)
+
+# validate secret
+@app.route('/validation/wait')
+def val_wait():
+	print("estou à espera")
+	# prepare thread
+	thread = threading.Thread(target=val_response)
+	thread.start()
+	secret_submitted.wait()
+	global responsible_user
+	global responsible_photoData
+	global responsible_photoType
+
+	if responsible_user == None:
+		print("*********************vou mandar nada")
+		return 'yo'
+
+	ret_html = """
+		<p><b>"""+responsible_user+"""</b> has just used your secret.</p>
+		<div class="col-xs-3">
+			<img src="data:"""+responsible_photoType+"""+;base64,"""+responsible_photoData+"""" align="right"/>
+		</div>
+	"""
+
+	# clear vars
+	responsible_user = None
+	responsible_photoData = None
+	responsible_photoType = None
+	
+	return ret_html
+
+	
+# validate secret
+@app.route('/validation/validate', methods=['POST'])
+def val_response():
+	reqSecret = request.form['secret']
+	name, key, photo_data, photo_type = Authentication()
+	flag_found=0
+	for user in user_list:
+		if user.secret==reqSecret:
+			flag_found=1
+			params = {'access_token': user.access_token}
+			resp = requests.get("https://fenix.tecnico.ulisboa.pt/api/fenix/v1/person", params = params)
+			if resp.status_code==200:
+				r_json = resp.json()
+				# clear secret
+				user.clearSecret()
+				global responsible_user
+				global responsible_photoData
+				global responsible_photoType
+				responsible_user = name
+				responsible_photoData = photo_data
+				responsible_photoType = photo_type
+				# now notice the searched user that their secret has been used
+				secret_submitted.set()
+				break
+	if(flag_found == 0):
+		return render_template("validate.html", notFound='yes', secret=reqSecret, username=name, key=key, photo_data = photo_data, photo_type=photo_type)	
+	return render_template("validate.html", username=name, key=key, photo_data = photo_data, photo_type=photo_type, secret=reqSecret, reqUser=r_json['name'], reqPhoto=r_json['photo']['data'], reqPhotoType=r_json['photo']['type'])
 
 def Authentication():
 	if 'key' in request.args:
@@ -91,7 +184,8 @@ def Authentication():
 					r_json = resp.json()
 					return r_json['name'], key, r_json['photo']['data'], r_json['photo']['type']
 	abort(401)
-	
+
+
 
 if __name__ == '__main__':
 
--- mobile/users.py
+++ mobile/users.py
@@ -4,3 +4,10 @@ class User:
 	def __init__(self, access_token):
 		self.access_token = access_token
 		self.key = str(uuid.uuid4())
+		self.secret = None
+
+	def updateSecret(self, secret):
+		self.secret = secret
+
+	def clearSecret(self):
+		self.secret = None
